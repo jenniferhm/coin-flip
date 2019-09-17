d1f00353eef0e456a44b5154cc04f1ec4b6fa43b
Jennifer Ma <jenniferma93@gmail.com>
2019-09-17 16:24:09 -0700

    refactored coin flip

--- src/App.js
+++ src/App.js
@@ -3,28 +3,37 @@ import './App.css';
 import Coin from './coin';
 import Button from './button';
 
-const heads = "https://catalog.usmint.gov/on/demandware.static/-/Sites-usm-master-catalog-us/default/dw461eb6c6/images/hi-res/coins/Gold-Coins/17xa_a.jpg";
-const tails = "https://cdn3.volusion.com/aujxd.ayqsr/v/vspfiles/photos/623932084735-2.jpg?1497431502";
 
 class App extends React.Component {
+  static defaultProps = {
+    coins: [
+      {
+        side: "head",
+        image: "https://catalog.usmint.gov/on/demandware.static/-/Sites-usm-master-catalog-us/default/dw461eb6c6/images/hi-res/coins/Gold-Coins/17xa_a.jpg"
+      },
+      {
+        side: "tail",
+        image: "https://cdn3.volusion.com/aujxd.ayqsr/v/vspfiles/photos/623932084735-2.jpg?1497431502"
+      }
+    ]
+  }
 
   constructor(props) {
     super(props);
-    this.state = { totalCount: 0, headsCount: 0, tailsCount: 0, image: "hidden" };
+    this.state = { totalCount: 0, headsCount: 0, tailsCount: 0, image: "" };
     this.clicker = this.clicker.bind(this);
   }
 
   clicker(evt) {
-    this.setState(st => ({totalCount: st.totalCount += 1}));
-    if (Math.random() > 0.5) {
-      this.setState(st => ({
-        headsCount: st.headsCount += 1, image: heads
-      }))
-    } else {
-      this.setState(st => ({
-        tailsCount: st.tailsCount += 1, image: tails
-      }))
-    }
+    const randomCoin = this.props.coins[Math.floor(Math.random() * this.props.coins.length)];
+    this.setState(st => {
+      return {
+        totalCount: st.totalCount += 1,
+        headsCount: st.headsCount + (randomCoin.side === "head" ? 1 : 0),
+        tailsCount: st.tailsCount + (randomCoin.side === "tail" ? 1 : 0),
+        image: randomCoin.image
+      };
+    });
   }
 
   render() {
@@ -32,7 +41,7 @@ class App extends React.Component {
       <div className="App">
         <h1>Let's flip a coin!</h1>
         <Coin image={this.state.image} />
-        <Button clicker={this.clicker}/>
+        <Button clicker={this.clicker} />
         <p>Out of {this.state.totalCount} flips, there have been {this.state.headsCount} heads and {this.state.tailsCount} tails.</p>
       </div>
     );
